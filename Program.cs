74da196c72fd91bf620ccbc9861aea94e34a7289
OlehDrivko <116021948+OlehDrivko@users.noreply.github.com>
2024-01-20 20:57:44 +0100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectOOP_OlehDrivko_w68340
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
